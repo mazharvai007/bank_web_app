02f12a4e15e2ccceace67eb6a8aa10774b46b922
mazhar <swopnil420@gmail.com>
2024-08-07 14:39:44 +0600

    Completed the nav and hero area

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
-import './App.css';
+import styles from './style';
+import {
+	Navbar,
+	Hero,
+	Stats,
+	Business,
+	Billing,
+	CarDeal,
+	Clients,
+	CTA,
+	Testimonials,
+	Footer,
+} from './components';
 
-function App() {
+const App = () => {
 	return (
-		<>
-			<h1 className="text-3xl font-bold underline">Bank</h1>
-		</>
+		<div className="bg-primary w-full overflow-hidden">
+			<header className={`${styles.paddingX} ${styles.flexCenter}`}>
+				<div className={`${styles.boxWidth} text-white`}>
+					<Navbar />
+				</div>
+			</header>
+
+			<section className={`${styles.flexStart}`}>
+				<Hero />
+			</section>
+
+			<section className={`${styles.flexStart} ${styles.paddingX}`}>
+				<div className={`${styles.boxWidth} text-white`}>
+					<Stats />
+					<Business />
+					<Billing />
+					<CarDeal />
+					<Testimonials />
+					<Clients />
+					<CTA />
+					<Footer />
+				</div>
+			</section>
+		</div>
 	);
-}
+};
 
 export default App;
---
+++ src/constants/index.js
@@ -0,0 +1,228 @@
+import {
+	people01,
+	people02,
+	people03,
+	facebook,
+	instagram,
+	linkedin,
+	twitter,
+	airbnb,
+	binance,
+	coinbase,
+	dropbox,
+	send,
+	shield,
+	star,
+} from '../assets';
+
+export const navLinks = [
+	{
+		id: 'home',
+		title: 'Home',
+		url: 'home',
+	},
+	{
+		id: 'about',
+		title: 'About Us',
+		url: 'about-us',
+	},
+	{
+		id: 'features',
+		title: 'Features',
+		url: 'features',
+	},
+	{
+		id: 'solution',
+		title: 'Solution',
+		url: 'solutions',
+	},
+];
+
+export const features = [
+	{
+		id: 'feature-1',
+		icon: star,
+		title: 'Rewards',
+		content:
+			'The best credit cards offer some tantalizing combinations of promotions and prizes',
+	},
+	{
+		id: 'feature-2',
+		icon: shield,
+		title: '100% Secured',
+		content:
+			'We take proactive steps make sure your information and transactions are secure.',
+	},
+	{
+		id: 'feature-3',
+		icon: send,
+		title: 'Balance Transfer',
+		content:
+			'A balance transfer credit card can save you a lot of money in interest charges.',
+	},
+];
+
+export const feedbackItems = [
+	{
+		id: 'feedback-1',
+		content:
+			'Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.',
+		img: people01,
+		name: 'Herman Jensen',
+		position: 'Founder & Leader',
+	},
+	{
+		id: 'feedback-2',
+		content:
+			"Money makes your life easier. If you're lucky to have it, you're lucky.",
+		img: people02,
+		name: 'Steve Mark',
+		position: 'Founder & Leader',
+	},
+	{
+		id: 'feedback-3',
+		content:
+			'It is usually people in the money business, finance, and international trade that are really rich.',
+		img: people03,
+		name: 'Kenn Gallagher',
+		position: 'Founder & Leader',
+	},
+];
+
+export const status = [
+	{
+		id: 'status-1',
+		title: 'User Active',
+		value: '3800+',
+	},
+	{
+		id: 'status-2',
+		title: 'TRUSTED BY COMPANY',
+		value: '230+',
+	},
+	{
+		id: 'status-3',
+		title: 'TRANSACTION',
+		value: '$230M+',
+	},
+];
+
+export const footerLinks = [
+	{
+		title: 'Useful Links',
+		links: [
+			{
+				id: 'useful-link-1',
+				name: 'Content',
+				link: 'https://www.hoobank.com/content/',
+			},
+			{
+				id: 'useful-link-2',
+				name: 'How it Works',
+				link: 'https://www.hoobank.com/how-it-works/',
+			},
+			{
+				id: 'useful-link-3',
+				name: 'Create',
+				link: 'https://www.hoobank.com/create/',
+			},
+			{
+				id: 'useful-link-4',
+				name: 'Explore',
+				link: 'https://www.hoobank.com/explore/',
+			},
+			{
+				id: 'useful-link-5',
+				name: 'Terms & Services',
+				link: 'https://www.hoobank.com/terms-and-services/',
+			},
+		],
+	},
+	{
+		title: 'Community',
+		links: [
+			{
+				id: 'community-1',
+				name: 'Help Center',
+				link: 'https://www.hoobank.com/help-center/',
+			},
+			{
+				id: 'community-2',
+				name: 'Partners',
+				link: 'https://www.hoobank.com/partners/',
+			},
+			{
+				id: 'community-3',
+				name: 'Suggestions',
+				link: 'https://www.hoobank.com/suggestions/',
+			},
+			{
+				id: 'community-4',
+				name: 'Blog',
+				link: 'https://www.hoobank.com/blog/',
+			},
+			{
+				id: 'community-5',
+				name: 'Newsletters',
+				link: 'https://www.hoobank.com/newsletters/',
+			},
+		],
+	},
+	{
+		title: 'Partner',
+		links: [
+			{
+				id: 'partner-1',
+				name: 'Our Partner',
+				link: 'https://www.hoobank.com/our-partner/',
+			},
+			{
+				id: 'partner-2',
+				name: 'Become a Partner',
+				link: 'https://www.hoobank.com/become-a-partner/',
+			},
+		],
+	},
+];
+
+export const socialMedia = [
+	{
+		id: 'social-media-1',
+		icon: instagram,
+		link: 'https://www.instagram.com/',
+	},
+	{
+		id: 'social-media-2',
+		icon: facebook,
+		link: 'https://www.facebook.com/',
+	},
+	{
+		id: 'social-media-3',
+		icon: twitter,
+		link: 'https://www.twitter.com/',
+	},
+	{
+		id: 'social-media-4',
+		icon: linkedin,
+		link: 'https://www.linkedin.com/',
+	},
+];
+
+export const clients = [
+	{
+		id: 'client-1',
+		logo: airbnb,
+	},
+	{
+		id: 'client-2',
+		logo: binance,
+	},
+	{
+		id: 'client-3',
+		logo: coinbase,
+	},
+	{
+		id: 'client-4',
+		logo: dropbox,
+	},
+];
